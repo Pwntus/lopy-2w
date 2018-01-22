b19cdab2604099596d1fe8a6fafaa8bed1f5cc51
Pontus Aurdal <pontus.edvard@gmail.com>
2018-01-22 12:22:45 +0100

    Reduce config file

--- lopy/config.py
+++ lopy/config.py
@@ -12,32 +12,3 @@ AWS_PRIVATE_KEY = '/flash/cert/privkey.pem'
 THING_NAME = "00001301"
 CLIENT_ID = "00001301"
 TOPIC = "$aws/things/00001301/shadow/update"
-
-################## Subscribe / Publish client #################
-#CLIENT_ID = 'PycomPublishClient'
-#TOPIC = '$aws/things/00001301/shadow/update'
-#OFFLINE_QUEUE_SIZE = -1
-#DRAINING_FREQ = 2
-#CONN_DISCONN_TIMEOUT = 10
-#MQTT_OPER_TIMEOUT = 5
-#LAST_WILL_TOPIC = '$aws/things/00001301/shadow/update'
-#LAST_WILL_MSG = 'To All: Last will message'
-
-####################### Shadow updater ########################
-#THING_NAME = "00001301"
-#CLIENT_ID = "00001301"
-#TOPIC = "$aws/things/00001301/shadow/update"
-#CONN_DISCONN_TIMEOUT = 10
-#MQTT_OPER_TIMEOUT = 5
-
-####################### Delta Listener ########################
-#THING_NAME = "my thing name"
-#CLIENT_ID = "DeltaListener"
-#CONN_DISCONN_TIMEOUT = 10
-#MQTT_OPER_TIMEOUT = 5
-
-####################### Shadow Echo ########################
-#THING_NAME = "00001301"
-#CLIENT_ID = "ShadowEcho"
-#CONN_DISCONN_TIMEOUT = 10
-#MQTT_OPER_TIMEOUT = 5
